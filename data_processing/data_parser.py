7bbee6041b7cecae6caf34ecced0322e3915ac67
soccerdroid <mguarand@espol.edu.ec>
2019-07-15 11:22:48 -0500

    processing data and transforming to final format for the force-d3 graph

--- data_processing/data_parser.py
+++ data_processing/data_parser.py
@@ -1,29 +1,46 @@
 import pandas as pd
 from utils import *
 import sys
+import logging
 
 filename = sys.argv[1]
 
 chunksize = 5000
+logging.basicConfig(format='%(asctime)s: [%(levelname)s - Process Data] =======> %(message)s', level=logging.INFO)
+
+total_df = pd.DataFrame()
 for chunk in pd.read_csv(filename, sep="\t", escapechar="\\", chunksize=chunksize):
 	#Step 1. Data cleansing and filtering
     #check if creativecommon.org is in the provider_domain and remove those rows
     df = chunk[chunk["provider_domain"]!="creativecommons.org"]
-    print("Creative Commons domains deleted")
 
     #Step 2. Extract the CC License
     df["license_type"] = df["cc_license"].apply(getLicense)
     
     #delete rows without a valid CC license
     df = df.dropna()
-    print(df[["provider_domain","license_type"]].head(3))
+    total_df = pd.concat([total_df,df])
 
-    #Step 3. Create a method to format the domain name in the 'provider_domain'$
-    #The following library may be helpful: tldextract
+total_df.to_csv("temp/"+filename,sep="\t", escapechar="\\", index=False)
+logging.info("END OF PRUNING")
+
+filename = expandToSourcesTargets("temp/"+filename)
+total_df = pd.DataFrame()
+for chunk in pd.read_csv(filename, sep="\t", escapechar="\\", chunksize=chunksize):
+	#Return to step 1: remove circular links
+	df = chunk[chunk["source"]!=chunk["target"]]
+
+	#Step 3. Create a method to format the domain name in the 'provider_domain'
+	df["source"] = df["source"].apply(getDomainName)
+
+	#Step 4. Aggregate the data (sum all images and total links) group by domain
+	df.groupby("source").sum()
+	total_df = pd.concat([total_df,df])
+
+convertToJSON(total_df)
 
 
-    #Step 4. Aggregate the data (sum all images and total links) group by 'prov$
-
+    
 
     #temporarily write to file
     #logging.info('Write output to file')
--- data_processing/utils.py
+++ data_processing/utils.py
@@ -1,5 +1,9 @@
 import re
 from tldextract import extract
+import json
+import logging
+
+LICENSES_NAMES = ["by","by-sa","by-nd","by-nc","by-nc-sa","by-nc-nd"]
 
 def getLicense(_path):
     '''
@@ -34,3 +38,52 @@ def getLicense(_path):
 def getDomainName(_provider_domain):
     res = extract(_provider_domain)
     return res.domain
+
+def CustomParser(data):
+    import json
+    json_data = json.loads(data)
+    return json_data
+
+
+#this could be added to the loadParquetFiles() function in ExtractData.py
+def expandToSourcesTargets(_file_path):
+    stem_filename = _file_path.split(".")[0]
+    out_file = open(stem_filename+"_extended.tsv","w")
+    out_file.write("source\tcc_license\timages\ttarget\tvalue\n")
+    with open(_file_path, 'r') as f:
+        f.readline()
+        while (True):
+            line = f.readline()
+            if line: 
+                first_occurrence = True
+                line = line.replace("\'", "\"")
+                fields = line.split("\t")
+                provider_domain = fields[0]
+                #column 4 is the links field
+                links = json.loads(fields.pop(3))
+                for k,v in links.items():
+                    if(not first_occurrence):
+                        fields[2] = "0"  #this is for not multiplying the initial images quantity every time we create a new row
+                    out_line = "\t".join(fields) #fields contain [provider_domain,cc_license,images]
+                    out_line = out_line.strip()
+                    out_line += k+"\t"+str(v)+"\n" #add the target and number of hrefs from the source to the target
+                    out_file.write(out_line)
+                    first_occurrence = False
+            else:
+                out_file.close()
+                logging.info("END OF FILE")
+                logging.info("END WRITING SOURCES-TARGETS FILE")
+                return(stem_filename+"_extended.tsv")
+    return None
+
+def convertToJSON(_df):
+    nodes_df = _df[["source","images"]]
+    nodes_df = nodes_df.rename(index=str, columns={"source":"id"})
+    nodes_dict = nodes_df.to_dict('records')
+    links_df = _df[["source","target","value"]]
+    links_dict = links_df.to_dict('records')
+    total_data = {"nodes":nodes_dict, "links":links_dict}
+    with open('graph_data_input_file.json', 'w') as file:
+        file.write(json.dumps(total_data)) 
+        logging.info("END")
+  
