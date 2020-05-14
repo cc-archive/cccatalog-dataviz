d1c707566c967eb4e83b47c022f4317cc28a6527
subham <subham.iitrpr@gmail.com>
2020-05-14 20:15:40 +0530

    Add two-dimensional visualization of the graph

---
+++ react-frontend/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Graph2D from './components/Graph2D';
+function App() {
+  return (
+    <div className="App">
+      <Graph2D />
+    </div>
+  );
+}
+
+export default App;
