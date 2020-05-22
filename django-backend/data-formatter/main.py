e8b86d60ee58cea7de11b79946f57e0bc94cf050
subham <subham.iitrpr@gmail.com>
2020-05-23 00:52:16 +0530

    Update Documentation

--- django-backend/data-formatter/main.py
+++ django-backend/data-formatter/main.py
@@ -1,43 +1,56 @@
 '''
-The following file converts the graph data schema, to distance list format.
-'''
-import json
-import copy
-import shelve
 
+The following file is a parser which converts the input data schema from {nodes:[], links:[]} to the distance list format.
+
+'''
+
+import shelve
+# For DeepCopy
+import copy
+# For JSON file output
+import json
+
+# INPUT DATA SOURCE FILENAME
 INPUT_FILE_NAME = 'fdg_input_file.json'
 OUTPUT_FILE_NAME = 'fdg_output_file'
+# Maximum distance to be considered
 MAX_DISTANCE = 10
 
 
-schema = {}
 
-for i in range(MAX_DISTANCE):
-    schema[str(i+1)] = []
+'''
+Builds schema for an individual node
+'''
+def buildSchema(distance):
+    schema = {}
+    for i in range(distance):
+        schema[str(i+1)] = []
+    return schema
+
+'''
+Converts the {'nodes': [], 'links': []} into Adjancency List
+'''
+def createAdjacencyList(aggregate_data):
+    links = aggregate_data['links']
+    adjacencyList = {}
+
+    for link in links:
+        key = link['source']
+        if not key in adjacencyList:
+            adjacencyList[key] = []
+        if not link['target'] in adjacencyList:
+            adjacencyList[link['target']] = []
+
+        adjacencyList[key].append({
+            "target": link['target'],
+            "value": link['value']
+        })
+    return adjacencyList
 
 
-input_file = open(INPUT_FILE_NAME).read()
-
-aggregate_data = json.loads(input_file)
-links = aggregate_data['links']
-nodes = aggregate_data['nodes']
-
-adjacencyList = {}
-
-for link in links:
-    key = link['source']
-    if not key in adjacencyList:
-        adjacencyList[key] = []
-    if not link['target'] in adjacencyList:
-        adjacencyList[link['target']] = []
-
-    adjacencyList[key].append({
-        "target": link['target'],
-        "value": link['value']
-    })
-
-
-
+'''
+Breadth First Search Traversal 
+'''
 def bfs(adjList, node):
     visited = []
     visited.append(node)
@@ -58,9 +71,31 @@ def bfs(adjList, node):
 
     return distanceList
 
+'''
+Creates a python dictionary and dumps a JSON object
+'''
+def dumpJSON(output_list):
+    json_output_list = {}
+    for key in output_list:
+        json_output_list[key] = output_list[key]
 
+    open(OUTPUT_FILE_NAME+'.json', 'w').write(json.dumps(json_output_list, indent=2))
+
+
+
+# Building schema
+schema = buildSchema(MAX_DISTANCE)
+
+# Loading Input File
+input_file = open(INPUT_FILE_NAME).read()
+aggregate_data = json.loads(input_file)
+# Building Adjacency List
+adjacencyList = createAdjacencyList(aggregate_data)
+
+# Opening shelve instance
 output_list = shelve.open(OUTPUT_FILE_NAME, writeback=True)
 
+# Calling BFS on every node and adding the nodes inside output_list
 for node in adjacencyList:
     # calling bfs from node
     distance_list_of_node = bfs(adjacencyList, node)
@@ -68,26 +103,26 @@ for node in adjacencyList:
     output_list[node] = distance_list_of_node
 
 
+# DEBUG: List to store the nodes which are are completely isolated (Both indegree and outdegree zero)
 redundant_nodes = []
 
-# Adding metadata
+nodes = aggregate_data['nodes']
+# Adding metadata to all nodes
 for node in nodes:
     try:
         # output_list[node['id']]['metadata'] = {key: value for key, value in node.items() if key not in 'id'}
         output_list[node['id']]['metadata'] = node
     except Exception as e:
+        # Appending all redundant nodes
         redundant_nodes.append(e.args[0])
 
-print('REDUNDANT_NODES: ', redundant_nodes, len(redundant_nodes))
+# DEBUG: print('REDUNDANT_NODES: ', redundant_nodes, len(redundant_nodes))
 
 output_list.sync()
 
-# Output JSON file
-json_output_list = {}
-for key in output_list:
-    json_output_list[key] = output_list[key]
+# Uncomment this to output JSON file too
+# dumpJSON(output_list)
 
-# open(OUTPUT_FILE_NAME+'.json', 'w').write(json.dumps(json_output_list, indent=2))
 
 output_list.close()
 
--- django-backend/src/api/views.py
+++ django-backend/src/api/views.py
@@ -5,21 +5,27 @@ import shelve
 MAX_DISTANCE = 10
 OUTPUT_FILE_NAME = 'fdg_output_file'
 
-
+'''
+Filters the Graph using nodeName and distance
+'''
 def getFilteredData(db, nodeName, distance):
-    nodeDistanceList = db[nodeName]
     nodes = []
     links = []
-    print(nodeDistanceList)
+    nodeDistanceList = db[nodeName]
+    # Iterating over all nodes which are at a distance of [1, distance] 
     for i in range(1, distance+1):
-        # link = nodeDistanceList[str(i)]
-        # link['source'] = nodeName
-        # links +=link
+        # For all links at a distance of "i"
         for j in nodeDistanceList[str(i)]:
+            # Appending to the links list
             links.append({**j, "source": nodeName})
+            # If "i" is at the last level then not adding distance one links
+            if(i == distance):
+                continue
+            # For all target nodes adding their distance one links
             for k in db[j['target']]['1']:
                 links.append({**k, "source": j['target'] })
-
+    
+    # Adding nodes metadata
     nodes.append(nodeDistanceList['metadata'])
     for i in links:
         if not i in nodes:
@@ -32,8 +38,9 @@ def getFilteredData(db, nodeName, distance):
 Serves Graph Data
 '''
 def serve_graph_data(request):
-    # Retrieving node name 
+    # Retrieving node name params
     nodeName = request.GET.get('name')
+
     # If node name is not provided sending whole file
     if(nodeName == None):
         data = open('api/data/'+OUTPUT_FILE_NAME+'.json', 'rb')
@@ -43,6 +50,7 @@ def serve_graph_data(request):
         if( not nodeName in db ):
             return JsonResponse({"error": True, "message": "node " + nodeName + " doesn't exist"}, json_dumps_params={'indent': 4})
 
+        # Retrieving distance params 
         distance = request.GET.get('distance')
         if(distance == None or int(distance) <=0):
             distance=MAX_DISTANCE
