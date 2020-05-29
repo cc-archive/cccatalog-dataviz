4fc5d364df2e77bb07303323762b2f4723ec4dc7
subham <subham.iitrpr@gmail.com>
2020-05-29 23:01:14 +0530

    Add CORS config

--- django-backend/src/linked_commons/settings.py
+++ django-backend/src/linked_commons/settings.py
@@ -38,16 +38,24 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000"
+    "https://ccdataviz.netlify.app/",
 ]
 
 ROOT_URLCONF = 'linked_commons.urls'
