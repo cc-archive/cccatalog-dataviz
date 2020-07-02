5f858ea5f480cec9b7745caa174955963622c692
subham <subham.iitrpr@gmail.com>
2020-07-02 18:38:27 +0530

    Update and optimize shelve dB generator script

--- django-backend/data-formatter/main.py
+++ django-backend/data-formatter/main.py
@@ -1,13 +1,16 @@
 import shelve
 import copy
 import json
+import datetime
 
 # INPUT DATA SOURCE FILENAME
 INPUT_FILE_NAME = 'fdg_input_file.json'
 OUTPUT_FILE_NAME = 'fdg_output_file'
+
 # Maximum distance to be considered
 MAX_DISTANCE = 10
 
+base_time = datetime.datetime.now()
 
 
 def build_schema(distance):
@@ -40,26 +43,38 @@ def create_adjacency_list(aggregate_data):
     return adjacency_list
 
 
-def bfs(adj_list, node):
+def build_distance_list(adj_list, node, visited, distance_list):
     """
-    Breadth First Search Traversal 
+    Traverses the component and builds/update the distance_list
     """
-    visited = []
-    visited.append(node)
-    distance_list = copy.deepcopy(schema)
+    visited.add(node)
     q = []
-    q.append({"node": node, "level": 0})
+    q.append({"node": node, "parent": []})
     while q:
         front = q.pop(0)
-        currNode = front['node']
-        currLevel = front['level']
-        if(int(currLevel) >= MAX_DISTANCE):
-            continue
-        for i in adj_list[currNode]:
-            if not i in visited:
-                q.append({'node': i['target'], 'level': currLevel+1})
-                distance_list[str(currLevel+1)].append(i)
-                visited.append(i)
+        curr_node = front['node']
+        parent = front['parent']
+        parent.append(curr_node)
+        # Keeping only last [MAX_DISTANCE] nodes
+        if(len(parent) > MAX_DISTANCE):
+            parent.pop(0)
+
+        # For every node directly connected [At distance one] from the curr_node
+        for i in adj_list[curr_node]:
+            # Updating the parent's distance_list
+            for j in enumerate(reversed(parent)):
+                if j[1] not in distance_list:
+                    distance_list[j[1]] = copy.deepcopy(schema)
+
+                if j[0]==0:
+                  distance_list[j[1]]['1'].append(i)
+                else:
+                  distance_list[j[1]][str(j[0]+1)].append(i['target'])
+            
+            # Checking if the node is already already travered or not
+            if not i['target'] in visited:
+                q.append({'node': i['target'], 'parent': parent})
+                visited.add(i['target'])
 
     return distance_list
 
@@ -74,6 +89,9 @@ def dump_json(output_list):
     open(OUTPUT_FILE_NAME+'.json', 'w').write(json.dumps(json_output_list, indent=2))
 
 
+def display_time(message):
+    curr_time = datetime.datetime.now()
+    print(message + " : ", curr_time - base_time)
 
 # Building schema
 schema = build_schema(MAX_DISTANCE)
@@ -81,40 +99,51 @@ schema = build_schema(MAX_DISTANCE)
 # Loading Input File
 input_file = open(INPUT_FILE_NAME).read()
 aggregate_data = json.loads(input_file)
+curr_time = datetime.datetime.now()
+display_time("Data Loaded")
+
 # Building Adjacency List
 adjacency_list = create_adjacency_list(aggregate_data)
+curr_time = datetime.datetime.now()
+display_time("Adjacency List Successfuly Built")
+
+temp_output_list = {}
+
+# Set to store the visited nodes
+visited = set()
+
+# Calling build_distance_list on every node which is not yet traversed
+for node in adjacency_list:
+    if node not in visited:
+        # calling build_distance_list from node
+        build_distance_list(adjacency_list, node, visited, temp_output_list)
+
+display_time("Distance List Successfuly Built")
+
 
 # Opening shelve instance
-output_list = shelve.open(OUTPUT_FILE_NAME, writeback=True)
-
-# Calling BFS on every node and adding the nodes inside output_list
-for node in adjacency_list:
-    # calling bfs from node
-    distance_list_of_node = bfs(adjacency_list, node)
-    # adding to output_list
-    output_list[node] = distance_list_of_node
-
+output_list = shelve.open(OUTPUT_FILE_NAME)
 
 # DEBUG: List to store the nodes which are are completely isolated (Both indegree and outdegree zero)
-redundant_nodes = []
+zero_indeg_nodes = []
 
 nodes = aggregate_data['nodes']
 # Adding metadata to all nodes
 for node in nodes:
-    try:
-        # output_list[node['id']]['metadata'] = {key: value for key, value in node.items() if key not in 'id'}
-        output_list[node['id']]['metadata'] = node
-    except Exception as e:
-        # Appending all redundant nodes
-        redundant_nodes.append(e.args[0])
+    if node['id'] not in temp_output_list:
+        temp_output_list[node['id']] = copy.deepcopy(schema)
+        zero_indeg_nodes.append(node['id'])
+    temp_output_list[node['id']]['metadata'] = node
+    output_list[node['id']] = temp_output_list[node['id']]
+
 
 # DEBUG: 
-# print('REDUNDANT_NODES: ', redundant_nodes, len(redundant_nodes))
+# print('zero_indeg_nodes: ', len(zero_indeg_nodes))
 
-output_list.sync()
+display_time("Nodes Metadata Successfully Added")
 
 # Uncomment this to output JSON file [Only to visualize the data]
-# dump_json(output_list)
-
+# dump_json(temp_output_list)
 
 output_list.close()
+display_time("Operation Successful")
