a75f91b020eee2f59a21f67531c9720e10ab0eac
subham <subham.iitrpr@gmail.com>
2020-08-05 23:56:13 +0530

    Update build script database logic from shelve to mongodb
    Add docker-compose.prod.yml

--- backend/scripts/build_adj_list_db.py
+++ backend/scripts/build_adj_list_db.py
@@ -1,12 +1,18 @@
-import shelve
 import json
 import datetime
+import pymongo
+import os
 
 DIST = 'D' # Prefix in every node distance key [eg. D1, D2]
-REV_DIST = 'RD' 
+REV_DIST = 'RD'
 DBNAME = "graph_dB"
 INPUT_FILENAME = 'fdg_input_file.json'
 base_time = datetime.datetime.now()
+
+USERNAME=os.environ.get('MONGO_INITDB_ROOT_USERNAME')
+PASSWORD=os.environ.get('MONGO_INITDB_ROOT_PASSWORD')
+MONGO_DB_NAME=os.environ.get('MONGO_DB_NAME')
+MONGO_COLLECTION_NAME=os.environ.get('MONGO_COLLECTION_NAME')
 
 
 def main(
@@ -30,10 +36,28 @@ def init_adjacency_shelf(adjacency_shelf_name, aggregate_data):
         f'{datetime.datetime.now() - base_time}'
         f' Saving Adjacency map to shelf DB: {adjacency_shelf_name}'
     )
+    client = pymongo.MongoClient(f'mongodb://{USERNAME}:{PASSWORD}@localhost:27017')
+    db = client.get_database(name=MONGO_DB_NAME)
+    node_collection = db.get_collection(name=MONGO_COLLECTION_NAME)
+    count=0
+    curr_batch=[]
+    for k in adjacency_map:
+        curr_batch.append({"_id": k, **adjacency_map[k]})
+        count+=1
+        if(count%1000==0):
+            node_collection.insert_many(curr_batch)
+            curr_batch.clear()
+            print(
+                f'{datetime.datetime.now()-base_time}',
+                f'Added {count} nodes'
+            )
 
-    with shelve.open(adjacency_shelf_name) as db:
-        for k in adjacency_map:
-            db[k] = adjacency_map[k]
+    # Adding elems which are left
+    node_collection.insert_many(curr_batch)
+    print(
+        f'{datetime.datetime.now()-base_time}',
+        f'Added {count} nodes'
+    )
     print(
         f'{datetime.datetime.now() - base_time}'
         f' Adjacency Map saved'
@@ -78,9 +102,13 @@ def add_node_metadata(adjacency_map, aggregate_data):
     
     nodes = aggregate_data['nodes']
     for node in nodes:
+        curr_node = node
+        if(curr_node['cc_licenses']):
+            curr_node['cc_licenses']=json.dumps(curr_node['cc_licenses'])
+
         adjacency_map.setdefault(
             node['id'], {dist_1_out_key: [], dist_1_in_key:[]}
-        )['metadata'] = node
+        )['metadata'] = curr_node
 
     print(
         f'{datetime.datetime.now() - base_time}'
